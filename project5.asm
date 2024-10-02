20856de39a94094076d7024ad59a822ff56e9b11
wikim00 <wikim00@gmail.com>
2024-10-02 16:13:34 -0700

    project 5

--- project5.asm
+++ project5.asm
@@ -1,59 +1,60 @@
 .data
-prompt1: .asciiz "Enter an integer n: "
-prompt2: .asciiz "The sum of numbers from 1 to n is: "
-newline: .asciiz "\n"
+strPromptFirst: .asciiz "Enter an integer value n:"
+strResult: .asciiz "Sum is: "
+strCR: .asciiz "\n"
 
 .text
+.global main
+
 main:
-    # Prompt the user for an integer n
-    li $v0, 4
-    la $a0, prompt1
-    syscall
+    # STEP 1 -- get the first operand
+    # Print a prompt asking user for input
+    li $v0, 4              # syscall number 4 will print string whose address is in $a0
+    la $a0, strPromptFirst  # load address of the string
+    syscall                 # actually print the string
 
-    # Read the integer n from the user
-    li $v0, 5
-    syscall
-    move $a0, $v0
+    # Now read in the first operand
+    li $v0, 5              # syscall number 5 will read an int
+    syscall                 # actually read the int
+    move $s0, $v0          # save result in $s0 for later
 
-    # Call the recursive function to find the sum
-    jal recursive_sum
+    # STEP 2 -- Call the function to calculate the sum
+    move $a0, $s0          # move the input integer n to register $a0
+    jal sum                # jump to the function sum to calculate the sum
+    move $s1, $v0          # move the final sum value to register $s1
 
-    # Print the result
-    li $v0, 4
-    la $a0, prompt2
-    syscall
+    # STEP 3 -- print the sum
+    # First print the string prelude
+    li $v0, 4              # syscall number 4 -- print string
+    la $a0, strResult      # address of string
+    syscall                 # actually print the string
 
-    li $v0, 1
-    move $a0, $v0
-    syscall
+    # Then print the actual sum
+    li $v0, 1              # syscall number 1 -- print int
+    move $a0, $s1          # put the result in $a0 for print
+    syscall                 # actually print the int
 
-    li $v0, 4
-    la $a0, newline
-    syscall
+    # STEP 4 --
+    # Finally print a carriage return
+    li $v0, 4              # syscall for print string
+    la $a0, strCR          # address of string with a carriage return
+    syscall                 # actually print the string
 
-    # Exit the program
-    li $v0, 10
-    syscall
+    # STEP 5 -- exit
+    li $v0, 10             # syscall number 10 is to terminate the program
+    syscall                 # exit now
 
-recursive_sum:
-    # Base case: n is 0
-    beq $a0, $zero, return_zero
+# Sum Function
+sum:
+    # Input: $a0 = n (the integer to sum up to)
+    # Output: $v0 = sum of integers from 1 to n
+    move $v0, $zero        # Initialize sum to 0
+    move $t0, $zero        # Initialize counter to 0
 
-    # Recursive case: n is not 0
-    addi $sp, $sp, -4
-    sw $ra, 0($sp)
+    # Loop from 1 to n
+loop:
+    addi $t0, $t0, 1       # Increment counter
+    add $v0, $v0, $t0      # Add counter to sum
+    bne $t0, $a0, loop     # Repeat until counter equals n
 
-    addi $a0, $a0, -1
-    jal recursive_sum
-
-    lw $ra, 0($sp)
-    addi $sp, $sp, 4
-
-    add $v0, $v0, $a0
-    addi $v0, $v0, 1  # Add 1 to include the current value
-
-    jr $ra
-
-return_zero:
-    move $v0, $zero
-    jr $ra
+    jr $ra                  # Return to caller
